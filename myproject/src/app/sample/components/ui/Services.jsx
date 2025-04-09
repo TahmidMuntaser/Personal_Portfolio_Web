e9e787b6acc8d86d75ac6483c0012c59c148a856
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2025-04-09 18:03:18 +0600

    services component edited

---
+++ myproject/src/app/sample/components/ui/Services.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react'
+
+const Services = () => {
+    return(
+        <section className='py-10 bg-gray-1000'>
+            <div className='container mx-auto px-4'>
+                <h2 className='text-5xl font-bold text-center mb-8'>My Services</h2>
+            </div>
+
+        </section>
+    );
+};
+
+export default Services;
--- myproject/src/app/sample/page.jsx
+++ myproject/src/app/sample/page.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Navbar from './components/ui/Navbar';
 import CustomButton from './components/ui/Button';
 import Hero from './components/ui/Hero';
+import Services from './components/ui/Services'
+
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Services />
      
-
-
-
       
     </div>
   );
