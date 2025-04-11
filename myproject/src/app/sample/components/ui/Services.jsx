60b8ad805da942deea144af48a1bf067cdd0d4a1
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2025-04-11 20:50:32 +0600

    hover animation fixed

--- myproject/src/app/sample/components/ui/Services.jsx
+++ myproject/src/app/sample/components/ui/Services.jsx
@@ -6,7 +6,9 @@ const Services = ({ services }) => {
     return (
         <section className="py-10 bg-gray-1000">
             <div className="container mx-auto px-4">
-                <h2 className="text-center text-5xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-purple-800 to-purple-300">My Services</h2>
+                <h2 className="text-center text-5xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-purple-800 to-purple-300">
+                    My Services
+                </h2>
                 <p className="text-center text-gray-300 max-w-prose mx-auto mb-8">
                     A quick overview of what I can do for you. I focus on delivering 
                     high-quality, efficient solutions tailored to each project's needs.
@@ -15,9 +17,9 @@ const Services = ({ services }) => {
                     {services.map((service) => (
                         <div
                             key={service.id}
-                            className="bg-gray-1000 hover:bg-purple-800 p-8 rounded-lg shadow-md hover:shadow-lg transition min-h-4 border-b-[1px] border-purple-800"
+                            className="bg-gray-1000 hover:bg-purple-800 p-8 rounded-lg shadow-md hover:shadow-lg transition-all duration-700 ease-in-out transform hover:scale-105 overflow-hidden border-b-[1px] border-purple-800"
                         >
-                            <div className="grid grid-cols-2 items-center">
+                            <div className="grid grid-cols-2 items-center h-full">
                                 {/* Left Part: ID and Title */}
                                 <div className="flex items-center space-x-2">
                                     <span className="bg-purple-800 text-white text-xl font-bold rounded-full w-10 h-10 flex items-center justify-center">
