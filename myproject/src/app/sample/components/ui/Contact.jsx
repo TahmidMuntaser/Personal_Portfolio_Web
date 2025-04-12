89f9b2a157a7d202f6525be96364d2f8a7b9214f
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2025-04-12 11:45:57 +0600

    contact from added to the component

--- myproject/src/app/sample/components/ui/Contact.jsx
+++ myproject/src/app/sample/components/ui/Contact.jsx
@@ -1,9 +1,21 @@
 import React from "react";  
 
 const Contact = () => {
-    
+    return(
+        <section className="bg-black min-h-screen px-5 py-16 md:px-20 text-white">
+            <div className="flex flex-col md:flex-row justify-between items-start gap-10">
+                {/* info */}
+                <div className="bg-gradient-to-b from-purple-900/30 to-purple-900/15 p-10 rounded-2xl flex-1">
+                    <h2></h2>
+                    <p></p>
+                    <form action=""></form>
+                </div>
 
-
-}
+                {/* details */}
+            
+            </div>
+        </section>
+    );
+};
 
 export default Contact;
