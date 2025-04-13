a879decda1e5d0a2aa95e87715a3e849ed6b2200
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2025-04-13 21:01:39 +0600

    footer style changed

--- myproject/src/app/sample/components/ui/Footer.jsx
+++ myproject/src/app/sample/components/ui/Footer.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
 const Footer = () => {
-    <footer> 
-        <div>
-            <p></p>
+    return(
+        <footer className="bg-gradient-to-b from-purple-900/45 to-purple-900/20 text-gray-400 py-10 px-4 md:px-20 mt-20"> 
+            <div className="flex flex-col md:flex-row justify-between items-center gap-6">
+                <p>© {new Date().getFullYear()} All rights reserved by Tahmid</p>
 
-            <div>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
+                <div>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
             </div>
-        </div>
-    </footer>
-}
+        </footer>
+    );
+};
 
 export default Footer;
