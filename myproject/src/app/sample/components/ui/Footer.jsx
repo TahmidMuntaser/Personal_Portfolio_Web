099375bdbf19d0b956b39e50c5215535bf132739
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2025-04-13 20:50:55 +0600

    Tag added to the footer

--- myproject/src/app/sample/components/ui/Footer.jsx
+++ myproject/src/app/sample/components/ui/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Footer = () => {
+    <footer> 
+        <div>
+            <p></p>
 
+            <div>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+        </div>
+    </footer>
 }
 
 export default Footer;
