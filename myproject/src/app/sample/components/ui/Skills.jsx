2e3c5ee499eeee752bc5322ad327eda36bc61070
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2025-04-14 21:12:52 +0600

    skills component updated

--- myproject/src/app/sample/components/ui/Skills.jsx
+++ myproject/src/app/sample/components/ui/Skills.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Skills = () => {
+    <section id="skills">
+        <div>
+            <h2>
 
-}
+            </h2>
+            <p>
+                
+            </p>
+        </div>
+    </section>
+};
 
 export default Skills;
