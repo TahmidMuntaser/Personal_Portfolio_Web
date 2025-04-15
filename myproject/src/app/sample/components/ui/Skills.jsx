74920297861c7877267201dce766db0239750e07
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2025-04-15 22:32:55 +0600

    skills component upd-0.4

--- myproject/src/app/sample/components/ui/Skills.jsx
+++ myproject/src/app/sample/components/ui/Skills.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Skills = ({ skills }) => {
     return(
-        <section id="skills" className="bg-gradient-to-b from-purple-900/45 to-purple-900/20 py-20 px-4 md:px-20">
+        <section id="skills" className="bg-gradient-to-b from-purple-900/45 to-purple-900/20 py-20 px-4 md:px-20  scroll-mt-52">
             <div className="text-center mb-12">
                 <h2 className="text-5xl font-bold mb-4 bg-gradient-to-r from-purple-800 to-purple-100 bg-clip-text" style={{ WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>
                     My Skills
@@ -14,7 +14,7 @@ const Skills = ({ skills }) => {
                 {skills.map((skill, index) => (
                 <div
                     key={index}
-                    className="bg-gray-900 hover:border-purple-500 border border-gray-700 rounded-xl p-8 flex flex-col items-center justify-center transition-all duration-300"
+                    className="bg-gray-950 hover:border-purple-500 border border-gray-700 rounded-xl p-8 flex flex-col items-center justify-center transition-all duration-300"
                 >
                     <div className="text-purple-400 mb-4">{skill.icon}</div>
                     <p className="text-white font-semibold">{skill.name}</p>
