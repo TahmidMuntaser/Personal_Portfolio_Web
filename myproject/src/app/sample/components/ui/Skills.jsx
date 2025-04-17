0cc351bdf1e58d7395e9de3c630fe9676edfa94f
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2025-04-17 19:44:18 +0600

    wroks.jsx header added

--- myproject/src/app/sample/components/ui/Skills.jsx
+++ myproject/src/app/sample/components/ui/Skills.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Skills = ({ skills }) => {
     return(
-        <section id="skills" className="bg-gradient-to-b from-purple-900/45 to-purple-900/20 py-20 px-4 md:px-20  scroll-mt-52">
+        <section id="skills" className="bg-gradient-to-b from-purple-900/45 to-purple-900/20 py-20 px-4 md:px-20 scroll-mt-52">
             <div className="text-center mb-12">
                 <h2 className="text-5xl font-bold mb-4 bg-gradient-to-r from-purple-800 to-purple-100 bg-clip-text" style={{ WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>
                     My Skills
--- myproject/src/app/sample/components/ui/Works.jsx
+++ myproject/src/app/sample/components/ui/Works.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Works = () => {
     return(
-        <section id='works'>
-            <div>
-                <h2></h2>
+        <section id='works' className='text-white py-16 px-4 md:px-16 bg-gradient-to-b from-purple-900/45 to-purple-900/20'>
+            <div className="text-center mb-10">
+                <h2 className='text-5xl font-bold mb-4 bg-gradient-to-r from-purple-800 to-purple-100 bg-clip-text' style={{ WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>My Recent Works</h2>
                 <p></p>
             </div>
 
