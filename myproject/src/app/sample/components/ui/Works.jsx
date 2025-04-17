17b78a6837a9d9ab2264743b8c816bb4b271e7c6
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2025-04-17 19:33:40 +0600

    Works.jsx file updated 1.1

--- myproject/src/app/sample/components/ui/Works.jsx
+++ myproject/src/app/sample/components/ui/Works.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Works = () => {
+    return(
+        <section id='works'>
+            <div>
+                <h2></h2>
+                <p></p>
+            </div>
 
-}
+            <div>
+                
+            </div>
+        </section>
+    );
+};
 
 export default Works;
